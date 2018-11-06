68ae262a280d5f20cb9ed38f6fee64a371f3407e
AndreyHaas <32494303+AndreyHaas@users.noreply.github.com>
2018-11-06 20:41:08 +0700

    refactoring

--- src/main/java/com/jcourse/gaas/firstlession/tasks/second/Main.java
+++ src/main/java/com/jcourse/gaas/firstlession/tasks/second/Main.java
@@ -3,6 +3,6 @@ package com.jcourse.gaas.firstlession.tasks.second;
 public class Main {
     public static void main(String[] args) {
         NumberGuesser numberGuesser = new NumberGuesser();
-        numberGuesser.game();
+        numberGuesser.run();
     }
 }
