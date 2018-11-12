3ad802c7483e7ba10e312e7ac85f600f80b8f7fb
AndreyHaas <32494303+AndreyHaas@users.noreply.github.com>
2018-11-13 00:35:38 +0700

    add tests to project

---
+++ src/test/java/CalcTest.java
@@ -0,0 +1,42 @@
+import com.jcourse.gaas.stackcalc.command.Addition;
+import com.jcourse.gaas.stackcalc.command.Command;
+import com.jcourse.gaas.stackcalc.command.Division;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalcTest {
+    private Stack<Double> stack = new Stack<>();
+    private Map<String, Double> map = new HashMap<>();
+
+    @Test
+    @DisplayName("Test for additional")
+    public void addTest() {
+        Command add = new Addition();
+        double firstValue = 5.0;
+        stack.push(firstValue);
+        double secondValue = 4.0;
+        stack.push(secondValue);
+        add.execute(stack, map, new String[0]);
+        assertEquals(9.0, stack.peek(), .000001);
+    }
+
+    @Test
+    @DisplayName("Test for division")
+    @RepeatedTest(3)
+    public void divTest() {
+        Command div = new Division();
+        double firstValue = 4.0;
+        stack.push(firstValue);
+        double secondValue = 4.0;
+        stack.push(secondValue);
+        div.execute(stack, map, new String[0]);
+        assertEquals(1.0, stack.peek(), .000001);
+    }
+}
