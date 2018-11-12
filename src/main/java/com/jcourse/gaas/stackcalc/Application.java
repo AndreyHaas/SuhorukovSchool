ec2b86e76288370a1b39e4cf3b6a8c4ff5931ddf
AndreyHaas <32494303+AndreyHaas@users.noreply.github.com>
2018-11-12 23:24:56 +0700

    refactoring

--- src/main/java/com/jcourse/gaas/stackcalc/Application.java
+++ src/main/java/com/jcourse/gaas/stackcalc/Application.java
@@ -1,68 +1,10 @@
 package com.jcourse.gaas.stackcalc;
 
-import com.jcourse.gaas.stackcalc.command.*;
-import org.apache.log4j.Logger;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Stack;
+import com.jcourse.gaas.stackcalc.common.FileReader;
 
 public class Application {
-    private static Logger log = Logger.getRootLogger();
-
     public static void main(String[] args) {
-        Stack<Double> stack = new Stack<>();
-        Scanner scanner = null;
-
-        Map<String, Command> commandsMap = new HashMap<>();
-        commandsMap.put("push", new Push());
-        commandsMap.put("print", new Print());
-        commandsMap.put("pop", new Pop());
-        commandsMap.put("add", new Addition());
-        commandsMap.put("define", new Define());
-        commandsMap.put("div", new Division());
-        commandsMap.put("sqrt", new Sqrt());
-        commandsMap.put("multi", new Multiplication());
-        commandsMap.put("sub", new Subtraction());
-
-
-        Map<String, Double> variableMap = new HashMap<>();
-        String s;
-
-        while (scanner.hasNext()) {
-            s = scanner.nextLine();
-            String[] str = s.split(" ");
-
-            if (commandsMap.containsKey(str[0])) {
-                try {
-                    commandsMap.get(str[0]).execute(stack, variableMap, str);
-                } catch (NumberFormatException ex) {
-                    log.error(ex);
-                }
-            } else {
-                log.error("Unregistered command in commandsMap");
-            }
-        }
-    }
-
-    private void getResource(String[] args) {
-        Scanner scanner = null;
-
-        if (args.length > 0) {
-            try {
-                scanner = new Scanner(new FileInputStream(args[0]));
-            } catch (FileNotFoundException ex) {
-                log.error(ex);
-            } finally {
-                if (scanner != null) {
-                    scanner.close();
-                }
-            }
-        } else {
-            scanner = new Scanner(System.in);
-        }
+        FileReader reader = new FileReader();
+        reader.readFile();
     }
 }
---
+++ src/main/java/com/jcourse/gaas/stackcalc/common/FileReader.java
@@ -0,0 +1,58 @@
+package com.jcourse.gaas.stackcalc.common;
+
+import com.jcourse.gaas.stackcalc.command.*;
+import org.apache.log4j.Logger;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class FileReader {
+    private static Logger LOG = Logger.getRootLogger();
+
+    public void readFile() {
+        Map<String, Double> variableMap = new HashMap<>();
+        Stack<Double> stack = new Stack<>();
+        String s;
+        Scanner scanner = null;
+        String fileName = "C:\\Users\\Andreas Haas\\IdeaProjects\\SuhorukovSchool\\src\\main\\resources\\source.txt";
+
+        try {
+            scanner = new Scanner(new FileInputStream(fileName));
+        } catch (
+                FileNotFoundException ex) {
+            LOG.error(ex);
+        }
+
+        if (scanner != null) {
+            while (scanner.hasNext()) {
+                s = scanner.nextLine();
+                String[] str = s.split(" ");
+
+                Map<String, Command> commandsMap = new HashMap<>();
+                commandsMap.put("push", new Push());
+                commandsMap.put("print", new Print());
+                commandsMap.put("pop", new Pop());
+                commandsMap.put("add", new Addition());
+                commandsMap.put("define", new Define());
+                commandsMap.put("div", new Division());
+                commandsMap.put("sqrt", new Sqrt());
+                commandsMap.put("multi", new Multiplication());
+                commandsMap.put("sub", new Subtraction());
+
+                if (commandsMap.containsKey(str[0])) {
+                    try {
+                        commandsMap.get(str[0]).execute(stack, variableMap, str);
+                    } catch (NumberFormatException ex) {
+                        LOG.error(ex);
+                    }
+                } else {
+                    LOG.error("Unregistered command in commandsMap");
+                }
+            }
+        }
+    }
+}
