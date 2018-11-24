776f1cb35edb6a9ea77348ae8ff60622962087cd
AndreyHaas <32494303+AndreyHaas@users.noreply.github.com>
2018-11-24 20:48:38 +0700

    add lambda

--- src/main/java/com/jcourse/gaas/html/HtmlGen.java
+++ src/main/java/com/jcourse/gaas/html/HtmlGen.java
@@ -13,11 +13,13 @@ public class HtmlGen {
                 Arrays.sort(filesInDirectory, (fileOne, fileTwo) -> fileOne.getName().compareTo(fileTwo.getName()));
             }
 
-            for (File file : filesInDirectory) {
-                Date date = new Date(file.lastModified());
-                System.out.println("file name: " + file.getName());
-                System.out.println("last modified file date: " + date);
-                System.out.println("file length: " + file.length());
+            if (filesInDirectory != null) {
+                for (File file : filesInDirectory) {
+                    Date date = new Date(file.lastModified());
+                    System.out.println("file name: " + file.getName());
+                    System.out.println("last modified file date: " + date);
+                    System.out.println("file length: " + file.length());
+                }
             }
         }
     }
